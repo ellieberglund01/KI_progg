6c5d0f7c08d4f83106d86861bac989858644c6fc
ellieberglund01 <ellieber2001@gmail.com>
2024-02-07 11:00:46 +0100

    SISTE VERSJON KJØR

--- working_file.py
+++ working_file.py
@@ -1,5 +1,4 @@
 import numpy as np
-import copy
 import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
@@ -17,20 +16,17 @@ class CONSYS:
             self.controller.get_jaxnet_params(self.controller.layers)
         
         gradfunc = jax.value_and_grad(self.run_one_epoch, argnums=0) 
-        gradfunc = jax.jit(gradfunc, static_argnums=[1])
         mse_history = []
-
         for epoch in range(1,num_epochs):
-            self.plant.reset() #Gjør nada
+            self.plant.reset()
             self.controller.error_history = jnp.zeros(timesteps)
             mse, gradients = gradfunc(self.controller.get_params(), timesteps) #get_params?
             mse_history.append(mse)
-            print(epoch, mse)
             self.controller.update_params(self.learning_rate,gradients)
             #params = self.controller.params
                 
+        print(mse_history)
         self.controller.plot(mse_history)
-        self.controller.plotk()
         return mse_history
 
     def run_one_epoch(self, params, timesteps):
@@ -38,48 +34,21 @@ class CONSYS:
         prev_error = 0
         integral = 0
         features = jnp.array([])
-        plant = copy.deepcopy(self.plant)
-        sse=0
 
         for t in range(timesteps):
-            plant_output = plant.update(U) 
-            error = plant.target - plant_output  
+            plant_output = self.plant.update(U) 
+            error = self.plant.target - plant_output  
             derivative = error - prev_error
             integral += error
             prev_error = error
-            sse += error**2
-           
             features = jnp.array([error, derivative, integral]) 
             U = self.controller.compute(params, features, t).reshape(-1)[0]
-        mse=sse/timesteps        
-        #mse = jnp.mean(jnp.square(self.controller.error_history))
+                
+        mse = jnp.mean(jnp.square(self.controller.error_history))
         return mse
-
-class Controller:
-    def __init__(self, timesteps):
-        self.timesteps = timesteps
-    def run_one_epoch(self, params, timesteps):
-        raise NotImplementedError("Must be implemented by subclass.")
-    def update_params(self,learning_rate, gradient):
-        raise NotImplementedError("Must be implemented by subclass.")
-
-    def plot(self, mse_history):
-    # Plot the progression of learning
-        plt.figure(figsize=(10, 6))
-        plt.plot(range(1, num_epochs), mse_history, marker='o', linestyle='-')
-        plt.title('Progression of Learning')
-        plt.xlabel('Epoch')
-        plt.ylabel('Mean Squared Error (MSE)')
-        plt.grid(True)
-        plt.show()
-
-    def plotk(self):
-        pass
-
-
-class PIDController(Controller):
+    
+class PIDController:
     def __init__(self, kp, ki, kd, timesteps):
-        super().__init__(timesteps)
         self.params = jnp.array([kp,ki,kd])
         self.error_history = jnp.zeros(timesteps)
         self.k1_his = []  # Store changes in k1 across epochs
@@ -93,26 +62,13 @@ class PIDController(Controller):
         kp = params[0]
         ki = params[1]
         kd = params[2]
-        #self.error_history = self.error_history.at[timestep].set(features[0])
+        self.error_history = self.error_history.at[timestep].set(features[0])
         p = kp*features[0]
         d = kd*features[1]
         i = ki*features[2]
         U = p+d+i
         return U
     
-    def plotk(self):
-        # Plot the changes in k parameters across epochs (Må bare være for PID)
-        plt.figure(figsize=(10, 6))
-        plt.plot(range(1, num_epochs), self.k1_his, marker='o', label='kp') #hvordan
-        plt.plot(range(1, num_epochs), self.k2_his, marker='o', label='ki')
-        plt.plot(range(1, num_epochs), self.k3_his, marker='o', label='kd')
-        plt.title('Changes to k parameters across epochs')
-        plt.xlabel('Epochs')
-        plt.ylabel('k-value')
-        plt.legend()
-        plt.grid(True)
-        plt.show()
-
     def update_params(self,learning_rate, gradients):
         self.params = [param-learning_rate*grad for param, grad in zip(self.params,gradients)]
 
@@ -120,28 +76,37 @@ class PIDController(Controller):
         self.k2_his.append(self.params[1])
         self.k3_his.append(self.params[2])
 
+    def plot(self, mse_history):
+        # Plot the progression of learning
+        plt.figure(figsize=(10, 6))
+        plt.plot(range(1, num_epochs), mse_history, marker='o', linestyle='-')
+        plt.title('Progression of Learning')
+        plt.xlabel('Epoch')
+        plt.ylabel('Mean Squared Error (MSE)')
+        plt.grid(True)
+        plt.show()
 
-class NeuralController(Controller):
-    def __init__(self, layers, timesteps, activation_function, lower, upper):
-        super().__init__(timesteps)
+         # Plot the changes in k parameters across epochs
+        plt.figure(figsize=(10, 6))
+        plt.plot(range(1, num_epochs), self.k1_his, marker='o', label='k1')
+        plt.plot(range(1, num_epochs), self.k2_his, marker='o', label='k2')
+        plt.plot(range(1, num_epochs), self.k3_his, marker='o', label='k3')
+        plt.title('Changes to k parameters across epochs')
+        plt.xlabel('Epochs')
+        plt.ylabel('k-value')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+class NeuralController:
+    def __init__(self, layers, timesteps, lower, upper):
         self.params = jnp.array([])
         self.layers = layers
         self.timesteps = timesteps
-        self.activation_function = self.get_activation_function(activation_function)
         self.error_history = jnp.zeros(timesteps)
         self.lower = lower
         self.upper = upper
         #self.activiation_functions = activation_functions
-
-    def get_activation_function(self,name):
-        if name.lower() == "sigmoid":
-            return lambda x: 1 / (1 + jnp.exp(-x))
-        elif name.lower() == "relu":
-            return lambda x: jnp.maximum(0, x)
-        elif name.lower() == "tanh":
-            return lambda x: jnp.tanh(x)
-        else:
-            raise ValueError(f"Activation function '{name}' is n ot supported.")
         
     def get_jaxnet_params(self, layers): #jax format?
         sender = layers[0]
@@ -162,16 +127,25 @@ class NeuralController(Controller):
         for (w, b), (dw, db) in zip(self.params, gradients)]
    
     def compute(self, all_params, features, timestep): 
+        def sigmoid(x):
+            return 1 / (1 + jnp.exp(-x))
         activations = features 
-        for i, (weights, biases) in enumerate(all_params):
-            if i == len(all_params) - 1:
-                # Remove sigmoid activation from the last layer (output layer)
-                activations = jnp.dot(activations, weights) + biases
-            else:
-                z = jnp.dot(activations, weights) + biases
-                activations = self.activation_function(z)
-        #self.error_history = self.error_history.at[timestep].set(features[0])
+        for weights, biases in all_params:
+            activations = sigmoid(jnp.dot(activations,weights) + biases)
+        print("ACTIVATIONS", activations)
+        self.error_history = self.error_history.at[timestep].set(features[0])
         return activations
+    
+    def plot(self, mse_history):
+        # Plot the progression of learning
+        plt.figure(figsize=(10, 6))
+        plt.plot(range(1,num_epochs), mse_history, marker='o', linestyle='-')
+        plt.title('Progression of Learning')
+        plt.xlabel('Epoch')
+        plt.ylabel('Mean Squared Error (MSE)')
+        plt.grid(True)
+        plt.show()
+
 
 #PLANTS:
 class BathTubPlant:
@@ -200,7 +174,7 @@ class BathTubPlant:
         return self.H
 
 class CournotPlant:
-    def __init__(self, pmax, c_m, T, lower, upper):
+    def __init__(self, pmax, c_m, T, lower, upper, ):
         self.target = T
         self.pmax = pmax
         self.c_m = c_m
@@ -219,8 +193,14 @@ class CournotPlant:
         self.q1 += U
         self.q2 += D
 
-        self.q1 = jnp.clip(self.q1,0,1)
-        self.q2 = jnp.clip(self.q2,0,1)
+        if self.q1 < 0:
+            self.q1=0
+        if self.q2 < 0:
+            self.q2=0
+        if self.q1 > 1:
+            self.q1=1
+        if self.q2 > 1:
+            self.q2=1
 
         q = self.q1 + self.q2
         price = self.pmax - q
@@ -229,61 +209,59 @@ class CournotPlant:
 
 
 #Funker ikke
-class HeaterRoomPlant: 
+class HeaterRoomPlant: #insulingregulering
     def __init__(self, initial_temp, target, lower_noise, upper_noise):
         self.temperature = initial_temp  # Initial room temperature
+
         self.lower_noise = lower_noise
         self.upper_noise = upper_noise
-        self.target = target
 
     def reset(self):
-        self.__init__(self.temperature, self.target, self.lower_noise, self.upper_noise)
+        self.__init__(self.temperature, self.lower_noise, self.upper_noise)
 
     def update(self, U):
         D = np.random.uniform(low=self.lower_noise, high=self.upper_noise)
-        delta_temperature = jnp.array(U) / 5.0 + D
-        new_temperature = self.temperature + delta_temperature
-        # Create a new instance of the object with the updated temperature
-        updated_plant = HeaterRoomPlant(new_temperature, self.target, self.lower_noise, self.upper_noise)
-        return updated_plant.temperature
+        # Simple room temperature model
+        delta_temperature = U / 10.0 + D
+        self.temperature += delta_temperature
+        return self.temperature
 
     def get_temperature(self):
         return self.temperature
 
-
 #FLYTT DETTE TIL MAIN
-kp = 0.001
-ki = 0.001
-kd = 0.001
+kp = 1
+ki = 0.1
+kd = 0.01
 
-num_epochs = 1000
+num_epochs = 50
+learning_rate = 0.01
 timesteps = 10
-learning_rate = 0.0001
 rangeNoiseLower = -0.01
 rangeNoiseUpper = 0.01
-lower = -0.01
-upper = 0.01
+lower = -0.1
+upper = 0.1
 
 #Bathtub
-A=100
-C=1
+A=1
+C=0.01
 H0=1
 
 #Cournotplant
-pmax = 3.5
-c_m = 0.1
-T = 3.2
+pmax = 10
+c_m = 0.1 
+T = 7
 
 #Heaterroom
 initial_temp = 20
-target = 22
 
 bathtub = BathTubPlant(A=A,C=C,H_0= H0, lower=rangeNoiseLower, upper=rangeNoiseUpper)
 cournotPlant = CournotPlant( pmax=pmax, c_m =c_m, T=T, lower=rangeNoiseLower, upper=rangeNoiseUpper)
-heater_room_plant = HeaterRoomPlant(initial_temp, target, rangeNoiseLower, rangeNoiseUpper)
+#heater_room_plant = HeaterRoomPlant(initial_temp, rangeNoiseLower, rangeNoiseUpper)
  
 pid_controller = PIDController(kp=kp,ki=ki,kd=ki, timesteps=timesteps)
-neural_controller = NeuralController(layers = [5,10,5,3], timesteps=timesteps, activation_function="sigmoid",lower=lower, upper=upper)
-consys = CONSYS(controller=neural_controller,plant=cournotPlant,learning_rate=learning_rate)
+neural_controller = NeuralController(layers = [5,10,5], timesteps=timesteps, lower=lower, upper=upper)
+
+consys = CONSYS(controller=neural_controller,plant=bathtub,learning_rate=learning_rate)
 consys.run_system(num_epochs,timesteps)
 
